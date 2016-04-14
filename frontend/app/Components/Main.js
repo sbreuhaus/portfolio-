2e8865a7700db6e232c9dbf76a7675ae2482ffea
Drew Gibson <gibsonhdrew@gmail.com>
2016-04-14 10:02:38 -0400

    main component

---
+++ frontend/app/Components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = React.createClass({
+  render: function(){
+    return (
+      <div className="main-container">
+
+      </div>
+    )
+  }
+});
+
+export default Main;
