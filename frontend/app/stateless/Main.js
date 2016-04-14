affbe42a21fe0be90d871d300135bedd5064705b
MichaelDM <michaelmachiah@gmail.com>
2016-04-14 11:39:47 -0400

    comiiting

--- frontend/app/stateless/Main.js
+++ frontend/app/stateless/Main.js
@@ -5,17 +5,24 @@ import {Link} from 'react-router';
 const Main = (props) => {
   return(
     <div className="site-external-wrapper">
+
       <nav>
-        <link to='/'>
-          <a href="#">HOME PAGE</a>
-        </link>
-        <Link to='/form'>
-          <a href="#">FORM PAGE</a>
+        <div>
+        <Link to='/'>
+          <div><span>HOME PAGE</span></div>
         </Link>
+      </div>
+      <div>
+        <Link to='/form'>
+          <div><span>FORM PAGE</span></div>
+        </Link>
+      </div>
       </nav>
+
       <main>
         {props.children}
       </main>
+
       <footer>
         <p>This is going to be our footer</p>
       </footer>
