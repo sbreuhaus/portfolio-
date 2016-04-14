433f8c41487419234637c9ecda7031360cf64f11
MichaelDM <michaelmachiah@gmail.com>
2016-04-14 10:13:13 -0400

    created Form and FormUI components (Mike)

---
+++ frontend/app/Config/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+
+// routing
+import Main from '../stateless/Main';
+import Home from '../Containers/Home';
+import Form from '../Containers/Form';
+
+const routes = {
+  <Router history={hashHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={Home}/>
+      <Route path='form' component={Form}/>
+    </Route>
+  </Router>
+};
+
+export default routes;
---
+++ frontend/app/Containers/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FormUI form '../Components/FormUI';
+
+const Form = React.createClass({
+  render (){
+    return {
+      <FormUI />
+    }
+  }
+})
+
+export default Form;
