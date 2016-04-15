08521b9555a97f8131e7f81bde47258e8139fb61
Drew Gibson <gibsonhdrew@gmail.com>
2016-04-14 20:57:54 -0400

    working on edit dropdown

--- backend/controllers/projects.js
+++ backend/controllers/projects.js
@@ -2,4 +2,48 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
+router.get('/', function(req, res){
+  projectsCollection.find().toArray(function(err, result){
+    if (err) {
+      console.log("ERROR!", err);
+      res.json("error");
+    } else if (result.length) {
+      res.json(result);
+    } else {
+      console.log('No document(s) found with defined "find" criteria');
+      res.json('no projects found');
+    }
+  });
+});
+
+router.post('/new', function(req, res){
+  projectsCollection.insert([req.body], function(err, data){
+    if (err) {
+      console.log(err);
+      res.json("error");
+    } else {
+      console.log('Inserted', data);
+      res.json(data);
+    }
+  });
+});
+
+router.put('/update', function(req, res){
+  projectsCollection.deleteMany({});
+  projectsCollection.insert([req.body], function(err, data){
+    if (err) {
+      console.log(err);
+      res.json("error");
+    } else {
+      console.log('Updated', data);
+      res.json(data);
+    }
+  });
+});
+
+router.delete('/delete', function(req, res){
+  console.log("received request");
+  projectsCollection.deleteMany({});
+});
+
 module.exports = router;
---
+++ frontend/app/Components/DeleteProjectUI.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DeleteProject = () => {
+  return (
+    <div>
+      <p>Delete Project</p>
+      <input placeholder="title"></input>
+      <input placeholder="image"></input>
+      <input placeholder="skills"></input>
+      <button type="button">Delete Project</button>
+    </div>
+  );
+};
+
+export default DeleteProject;
