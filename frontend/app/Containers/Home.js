d41fceec9a483abd80408ea7a555ea817c72b5c9
Alexandra Breuhaus <sbreuhaus@gmail.com>
2016-04-17 23:11:04 -0400

    night merge

--- frontend/app/Containers/Home.js
+++ frontend/app/Containers/Home.js
@@ -2,16 +2,16 @@ import React from 'react';
 import helpers from '../Utils/ajaxHelpers';
 import HomeUI from '../Components/HomeUI';
 import {Link} from 'react-router';
-import Project from '../Containers/Project';
-import ProjectUI from '../Components/ProjectUI';
-import DivStyles from '../stylesheets/project'
-
+import Project from '../Containers/Project'
+import routes from '../Config/routes';
 
 var origProjList;
 
 const Home = React.createClass({
   contextTypes: {
-    router: React.PropTypes.object.isRequired
+    router: function() {
+      return React.PropTypes.object.isRequired;
+    }
   },
   getInitialState (){
     return {
@@ -20,7 +20,7 @@ const Home = React.createClass({
       github: '',
       skills: [],
       projects: [],
-      origProj: [],
+      origProj: []
     }
   },
   componentDidMount(){
@@ -63,20 +63,18 @@ const Home = React.createClass({
   },
   handleListProjects(project, index, skills) {
     return (
-      <Link to='/project'>
-        <div className='projectItem' onClick={this.handleProjectSelect} key={index}>
-          <img className='projectImg' src={project.thumbnail}></img>
-          <div className='projectTitle'>{project.title}</div>
-          <div className='projSkills'>{project.skills.map(
-              function(obj, key){
-                return (
-                  <li className='skillPoint' key={key}>{obj}</li>
-                )
-              }
-            )}
-          </div>
+      <div className='projectItem' onClick={this.handleProjectSelect} key={index}>
+        <img className='projectImg' src={project.thumbnail}></img>
+        <div className='projectTitle'>{project.title}</div>
+        <div className='projSkills'>{project.skills.map(
+            function(obj, key){
+              return (
+                <li className='skillPoint' key={key}>{obj}</li>
+              )
+            }
+          )}
         </div>
-      </Link>
+      </div>
     )
   },
   handleSkillFilter(e){
@@ -84,7 +82,6 @@ const Home = React.createClass({
     for (var i = 0; i < this.state.origProj.length; i++) {
       if (this.state.origProj[i].skills.indexOf(e.target.innerText) > -1) {
         filteredProjs.push(this.state.origProj[i]);
-        console.log(this.state.origProj[i].skills);
       }
     }
     this.setState({
@@ -106,13 +103,49 @@ const Home = React.createClass({
         this.context.router.push({
           pathname: '/project',
           query: {
-            category: this.state.projects
+            sendTitle: this.state.projects[i].title,
+            sendDesc: this.state.projects[i].description,
+            sendImg: this.state.projects[i].image,
+            sendLink: this.state.projects[i].link
           }
         })
+        console.log(this.context);
       }
     }
   },
-
+  displayHomePage(){
+    return (
+      <div className='contentDisplay'>
+        <div className='navigatorDisplay'>
+          <div className='nameDisplay' onClick={this.handleRestoreProjects}>{this.state.logoName}</div>
+          <a className='linkedinDisplay' href={this.state.linkedIn}>Linkedin</a>&nbsp;&nbsp;&nbsp;&nbsp;
+          <a className='githubDisplay' href={this.state.github}>Github</a>
+        </div>
+        <ul className='skillList'>
+          {this.state.skills.map(this.handleListSkills)}
+        </ul>
+        <div className='projectsDisplay'>
+          {this.state.projects.map(this.handleListProjects)}
+        </div>
+        <footer className='footer'>
+          <p>Built with <b><a href='https://github.com/jumpingZebra/portfolio-'>Zebra Portfolio</a></b></p>
+        </footer>
+      </div>
+    );
+  },
+  displayProject(){
+    return (
+      <div className='prjPage'>
+        <h3 className='prjPageTitle'>Title</h3>
+        <Link to='/'>
+          <div className='backButton'>X</div>
+        </Link>
+        <p className='prjPageTxt'>This is going to be the content of the page etc etc etc etc  the content of the page etc etc etc etc  the content of the page etc etc etc etc  the content of the page etc etc etc etc e content of the page etc etc etc etc  the content of the page etc etc etc etc  the content of the page etc etc etc etc  the content of the pag 900 by 506 pixels for the image<br/>--</p>
+        <a className='prjPageLink'>Link</a><br/><br/>
+        <img className='prjPageImg' src='https://4kwallpapers.co/wp-content/uploads/2015/07/pink-sky-over-mountain-peak-4k-wallpaper.jpg'></img>
+      </div>
+    );
+  },
   render: function(){
     return (
       <div>
@@ -126,8 +159,9 @@ const Home = React.createClass({
           onListProjects={this.handleListProjects}
           onProjectSelect={this.handleProjectSelect}
           onRestoreProjects={this.handleRestoreProjects}
+          onDisplayHomePage={this.displayHomePage}
+          onDisplayProject={this.displayProject}
           />
-
       </div>
     );
   }
